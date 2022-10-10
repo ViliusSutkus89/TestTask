8ab0e39ae298ecbb778557fafcd6d71e75b6533e
Vilius Sutkus '89 <vilius@ViliusSutkus89.com>
2022-10-11 02:32:21 +0300

    Add login screen. Ugly for now, will beautify later

---
+++ app/src/main/java/com/viliussutkus89/codenet/tt/LoginScreen.kt
@@ -0,0 +1,170 @@
+package com.viliussutkus89.codenet.tt
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+private fun LoginScreenStateless(
+    email: String,
+    emailOnUpdate: (String) -> Unit = {},
+    emailOnFocusChange: (Boolean) -> Unit = {},
+    emailError: Boolean,
+
+    password: String,
+    passwordOnUpdate: (String) -> Unit = {},
+    passwordOnFocusChange: (Boolean) -> Unit = {},
+    passwordError: Boolean,
+
+    loginEnabled: Boolean,
+    onLogin: () -> Unit = {}
+) {
+    Column {
+        Text(
+            text = stringResource(R.string.login_title),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+
+        TextField(
+            value = email,
+            label = { Text(stringResource(R.string.login_label_email)) },
+            onValueChange = emailOnUpdate,
+            singleLine = true,
+            isError = emailError,
+            modifier = Modifier.onFocusChanged { emailOnFocusChange(it.isFocused) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        )
+
+        if (emailError) {
+            Text(stringResource(R.string.login_error_email_format))
+        }
+
+        TextField(
+            value = password,
+            label = { Text(stringResource(R.string.login_label_password)) },
+            onValueChange = passwordOnUpdate,
+            singleLine = true,
+            isError = passwordError,
+            modifier = Modifier.onFocusChanged { passwordOnFocusChange(it.isFocused) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        if (passwordError) {
+            Text(stringResource(R.string.login_error_password))
+        }
+
+
+        Button(
+            enabled = loginEnabled,
+            onClick = onLogin
+        ) {
+            Text(stringResource(R.string.login_button))
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewLoginScreenStateless() {
+    LoginScreenStateless(
+        email = "vilius@ViliusSutkus89.com",
+        emailError = false,
+        password = "123456",
+        passwordError = false,
+        loginEnabled = true
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewLoginScreenStateless2() {
+    LoginScreenStateless(
+        email = "",
+        emailError = true,
+        password = "123456",
+        passwordError = false,
+        loginEnabled = false
+    )
+}
+
+private enum class FocusStateTracker {
+    NeverFocused,
+    Focused,
+    NoLongerFocused
+}
+
+@Composable
+internal fun LoginScreen(
+    onLogin: (String, String) -> Unit
+) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var emailFocusTracker by rememberSaveable { mutableStateOf(FocusStateTracker.NeverFocused) }
+    // @TODO: this doesn't match ~@mydomain.com , but "~" is completely valid username
+    val emailIsValid = android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    val emailShowError = emailFocusTracker == FocusStateTracker.NoLongerFocused && !emailIsValid
+
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordFocusTracker by rememberSaveable { mutableStateOf(FocusStateTracker.NeverFocused) }
+    val passwordIsValid =  password.length >= 6
+    val passwordShowError = passwordFocusTracker == FocusStateTracker.NoLongerFocused && !passwordIsValid
+
+    LoginScreenStateless(
+        email = email,
+        emailError = emailShowError,
+        emailOnUpdate = { email = it },
+        emailOnFocusChange = {
+            if (it && emailFocusTracker == FocusStateTracker.NeverFocused) {
+                emailFocusTracker = FocusStateTracker.Focused
+            } else if (!it && emailFocusTracker == FocusStateTracker.Focused) {
+                emailFocusTracker = FocusStateTracker.NoLongerFocused
+            }
+         },
+
+        password = password,
+        passwordError = passwordShowError,
+        passwordOnUpdate = { password = it },
+        passwordOnFocusChange = {
+            if (it && passwordFocusTracker == FocusStateTracker.NeverFocused) {
+                passwordFocusTracker = FocusStateTracker.Focused
+            } else if (!it && passwordFocusTracker == FocusStateTracker.Focused) {
+                passwordFocusTracker = FocusStateTracker.NoLongerFocused
+            }
+        },
+
+        loginEnabled = !emailShowError && !passwordShowError,
+        onLogin = {
+            emailFocusTracker = FocusStateTracker.NoLongerFocused
+            passwordFocusTracker = FocusStateTracker.NoLongerFocused
+            if (emailIsValid && passwordIsValid) {
+                onLogin(email, password)
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewLoginScreen() {
+    LoginScreen { _, _ ->  }
+}
--- app/src/main/java/com/viliussutkus89/codenet/tt/MainActivity.kt
+++ app/src/main/java/com/viliussutkus89/codenet/tt/MainActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.viliussutkus89.codenet.tt.ui.theme.TestTaskTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +22,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TestTaskTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TestTaskComposeApp()
                 }
             }
         }
     }
 }
 
+internal class TestTaskViewModel: ViewModel() {
+    private val _isLoggedIn = MutableLiveData(false)
+    val isLoggedIn: LiveData<Boolean> = _isLoggedIn
+
+    fun login(email: String, password: String) {
+        _isLoggedIn.postValue(true)
+    }
+}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+internal fun TestTaskComposeApp(viewModel: TestTaskViewModel = TestTaskViewModel()) {
+    val isLoggedIn by viewModel.isLoggedIn.observeAsState(false)
+    if (isLoggedIn) {
+        Text("Placeholder for Search screen")
+    } else {
+        LoginScreen { email, password -> viewModel.login(email, password) }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TestTaskTheme {
-        Greeting("Android")
-    }
+private fun PreviewTestTaskComposeApp() {
+    TestTaskComposeApp()
 }
